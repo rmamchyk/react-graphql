5530c4098c8babf50db1931caba7ea11629ca87b
rmamchyk <roman.mamchyk@gmail.com>
2019-09-08 01:08:55 +0300

    initial setup of lyrics project

---
+++ lyrical-graphql/server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const schema = require('./schema/schema');
+
+const app = express();
+
+// Replace with your mongoLab URI
+const MONGO_URI = 'mongodb://admin:admin1@ds149606.mlab.com:49606/lyricaldb';
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoLab URI');
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+mongoose.connection
+    .once('open', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+app.use(bodyParser.json());
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;
