c9c62655daf8adb45395a7fddc9dc2eb361e8ae1
rmamchyk <roman.mamchyk@gmail.com>
2019-09-08 13:02:07 +0300

    implement SongCreate component

--- lyrical-graphql/client/components/SongList.js
+++ lyrical-graphql/client/components/SongList.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
   renderSongs() {
     return this.props.data.songs.map(song => {
       return (
         <li key={song.id} className="collection-item">
-          {song.title}
+          {song.title} 
         </li>
       )
     })
@@ -19,9 +20,14 @@ class SongList extends Component {
     }
 
     return (
-      <ul className="collection">
-        {this.renderSongs()}
-      </ul>
+      <div>
+        <ul className="collection">
+          {this.renderSongs()}
+        </ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons ">add</i>
+        </Link>
+      </div>
     )
   }
 }
